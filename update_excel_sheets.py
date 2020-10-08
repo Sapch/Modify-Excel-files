4d6108e625176dfc696a99f11a91fb6066b6a4cf
Sam <parham.chabi@ucalgary.ca>
2020-10-08 12:39:49 -0600

    Added modify_excel_values this function is tested and working

--- update_excel_sheets.py
+++ update_excel_sheets.py
@@ -2,21 +2,35 @@
 This module could be used to automate modification of Excel sheets
 TODO:
 complete functions:
-    - updating values of specific column in all sheet
     - updating column titles of all the sheets
 
-"""
+Working functions:
+    - updating values of specific column in all sheet
 
+"""
 import openpyxl as xl
 
+
 def modify_excel_values(name: "Excel file name", col: int, correction: int):
-    """ Modifies existing values of a specific column in Excel sheets
+    """ Modifies all values of a specific column in Excel sheets
 
     Arguments:
         name -- Excel file name
         col -- col number to be corrected
         correction -- a correction value between 0 to 1
     """
+    print(f"*** Excel file: {name} ***")
+    wb = xl.load_workbook(name)
+    for sheet in wb.worksheets:
+        print(f"working on {sheet}")
+        for row in range(2, sheet.max_row + 1):
+            current_value = sheet.cell(row, col).value
+            corrected_value = current_value * correction
+            sheet.cell(row, col).value = corrected_value
+            print(f"row{row}: {current_value} --> {corrected_value}")
+
+    wb.save(name)
+
 
 def modify_excel_sheet_col_titles(name: "Excel file name", new_titles: list):
     """ Modifies titles of columns in all of the sheets
